df87beb82f27b3d8745a79db5f6932ac1736837b
aliam93 <ali.asic.mougamadou@gmail.com>
2020-04-24 15:00:20 +0200

    app for book exo

--- flask/bookApp.py
+++ flask/bookApp.py
@@ -3,7 +3,7 @@
 
 from flask import Flask
 import json
-from os import path as os_path
+import pandas as pd
 app = Flask(__name__)
 
 dfBooks = pd.read_json("../flask/books.json")
@@ -14,14 +14,14 @@ def index():
 
 @app.route('/books')
 def books():
-    return dfBook.to_json(orient ='records')
+    return dfBooks.to_json(orient ='records')
 
 @app.route('/books/<book>')
 def load (book):
     if book is None:
         return ('please enter a valid isbn')
     else:
-	return dfBooks[dfBooks.isbn == book].to_dict()
+	return dfBooks[dfBooks.isbn == book].to_json(orient ='records')
 
 if __name__ == '__main__':
     app.run(debug=True) 
